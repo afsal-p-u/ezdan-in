483e8696401def008cfdca05549a0c240d0acb73
afsal p u <afsalpu07@gmail.com>
2024-12-08 00:49:43 +0530

    initial

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: [process.env.CLIENT_URL, "http://localhost:5599"], credentials: true }));
 
 const dbConnection = require('./utils/db.connection');
 dbConnection()
