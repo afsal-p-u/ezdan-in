6cfc9e9bcf025eccfa6468169b4b171e9c4d1e7e
afsal p u <afsalpu07@gmail.com>
2024-12-10 23:01:37 +0530

    v1 payment integration

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
-  // withCredentials: import.meta.env.VITE_WITHCREDENTIALS == "true" ? true : false, 
-  withCredentials: true, 
+  withCredentials: import.meta.env.VITE_WITHCREDENTIALS == true ? true : false, 
+  // withCredentials: true, 
 });
 
 export default apiRequest;
