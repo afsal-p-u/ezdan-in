4a03faaf2a142d5453ed9dac318b27a30fd7bf78
afsal p u <afsalpu07@gmail.com>
2024-12-10 22:29:08 +0530

    v1 payment integration

--- api/routes/payment.route.js
+++ api/routes/payment.route.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { CreateOrder } = require('../controllers/payment.controller');
 const { verifyUser } = require('../middleware/auth.middleware');
 
-router.post('/create', verifyUser, CreateOrder)   
+router.post('/create', CreateOrder)   
+// router.post('/create', verifyUser, CreateOrder)   
 
 module.exports = router
